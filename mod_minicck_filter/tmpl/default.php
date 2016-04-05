e35dd0662f55570149f25d205d21955aa07269d4
Arkadiy <a.sedelnikov@gmail.com>
2016-04-05 17:07:23 +0600

    Плагни 3.0.1 Модуль фильтра 1.3

--- mod_minicck_filter/tmpl/default.php
+++ mod_minicck_filter/tmpl/default.php
@@ -9,15 +9,19 @@
 
 defined('_JEXEC') or die;
 ?>
-
+<script>
+    function clearMinicckForm(){
+        jQuery(':checked, :selected, select','#mod-finder-searchform').not(':button, :submit, :reset, :hidden').removeAttr('checked').removeAttr('selected');
+    }
+</script>
 <form id="mod-finder-searchform" action="<?php echo $action; ?>" method="post" class="form-search">
-	<div class="minicck_filter<?php echo $moduleclass_sfx; ?>">
+	<div class="minicck_filter<?php echo $moduleclass_sfx; ?> row-fluid">
         <?php foreach($fields as $v) : ?>
             <?php echo $v; ?>
             <div style="clear: both;"></div>
         <?php endforeach; ?>
         <input type="submit" title="Submit" class="btn btn-primary btn-block"/>
-        <button class="btn btn-info btn-block" onclick="jQuery(':input','#mod-finder-searchform').not(':button, :submit, :reset, :hidden').val('').removeAttr('checked').removeAttr('selected');">
+        <button class="btn btn-info btn-block" onclick="clearMinicckForm();">
             <?php echo JText::_('MOD_MINICCK_RESET'); ?>
         </button>
 	</div>
